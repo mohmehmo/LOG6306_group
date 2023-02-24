9eeb2658805575d9d1c56c06fc80885d0f98d9e6
mmm <e.morovati@yahoo.com>
2023-02-24 11:46:05 -0500

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LOG6306 group Project
 Group project of LOG6306 course
+
+We use Python3.8 for our executions
--- get_repos_info.py
+++ get_repos_info.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def main():
-    repos = pd.read_csv("data/pyTorch/")
+    repos = pd.read_csv("data/keras/repos_keras_code.csv")
 
 if __name__ == '__main__':
     main()
