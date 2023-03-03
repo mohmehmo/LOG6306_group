bbbda86e5325592b4753d6b4d138a69a3428278f
mmm <e.morovati@yahoo.com>
2023-03-03 13:13:48 -0500

    adding new extracted dat

--- get_repo_info_api_v4.py
+++ get_repo_info_api_v4.py
@@ -8,10 +8,11 @@ import requests
 import json
 import csv
 
-access_token = ["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", # Ahura
+access_token = [
                 "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", #roozbeh
                 "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",
-                "149c667147dfb79d75c448355b7f031f856df65e"]
+                "149c667147dfb79d75c448355b7f031f856df65e"
+                ]
 
 access_token_counter = 0
 
@@ -20,7 +21,7 @@ def github_api_v3_call(url):
     global access_token_counter
     output = BytesIO()
     request = pycurl.Curl()
-    request.setopt(pycurl.HTTPHEADER, [f'Authorization: token {access_token[access_token_counter % 4]}'])
+    request.setopt(pycurl.HTTPHEADER, [f'Authorization: token {access_token[access_token_counter % 3]}'])
     request.setopt(request.URL, url)
     request.setopt(request.WRITEDATA, output)
     request.perform()
@@ -41,13 +42,7 @@ def check_repo_availability(repo_full_name):
     url = f"https://api.github.com/repos/{repo_full_name}"
     body = github_api_v3_call(url)
 
-    if body.get("message")== 'Not Found':
-        return False
-
-    if 'fork' not in body:
-        return False
-
-    if body['fork'] == True or body['disabled'] == True or body['private'] == True:
+    if body.get("message")== 'Not Found' or body.get("message") == 'Moved Permanently':
         return False
 
     return True
@@ -64,15 +59,15 @@ def run_query(query,token): # A simple function to use requests.post to make the
 
 def main():
     global access_token_counter
-    data_file_name = "data/keras/repos_keras_code_filtered(fork_star).csv"
+
+    data_file_name = "data/pyTorch/repos_pytorch_code_filtered(fork_star_activity).csv"
     repos = pd.read_csv(data_file_name, low_memory=False)
 
 
-
-    # # Backup output
-    # csvfile = open('data/pytorch/repos_pytorch_code_filtered(fork_star_activity_contributors)_commit_backup.csv', mode='w')
-    # writer = csv.writer(csvfile)
-    # writer.writerow(["id", "full_name", "url", "stars", "forks", "commit", "openIssue", "closedIssue"])
+    # Backup output
+    csvfile = open('data/temp/repos_pytorch_code_filtered(fork_star_activity)_commit_backup.csv', mode='w')
+    writer = csv.writer(csvfile)
+    writer.writerow(["id", "full_name", "url", "stars", "forks", "commit", "openIssue", "closedIssue"])
 
     repos_commits = []
     repos_openIssues = []
@@ -132,16 +127,9 @@ def main():
     repos["open_issue"] = repos_openIssues
     repos["closed_issue"] = repos_closedIssues
 
-    repos.to_csv('data/pytorch/repos_pytorch_code_filtered(fork_star_activity_contributors)_commit.csv', index=False)
+    repos.to_csv('data/pyTorch/repos_pytorch_code_filtered(fork_star_activity)_commit.csv', index=False)
     csvfile.close()
     print(f"Number of unavailable repos is  {unavailable_repos}")
-
-
-def main_new():
-    data_file_name = "data/keras/repos_keras_code_filtered(fork_star).csv"
-
-    repos = pd.read_csv(data_file_name, low_memory=False)
-
 
 
 if __name__ == '__main__':
